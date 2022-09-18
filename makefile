1809168ca219763966c01f738bb898c56009a507
cc <44751363+zzfn@users.noreply.github.com>
2022-09-18 21:20:59 +0800

    sad

---
+++ makefile
@@ -0,0 +1,52 @@
+# Binary name
+BINARY=drone-webhook
+GOBUILD=go build -o ${BINARY}
+GOCLEAN=go clean
+VERSION=0.0.2
+
+# Build
+build:
+	$(GOCLEAN)
+	$(GOBUILD)
+
+
+clean:
+	$(GOCLEAN)
+	$(RMTARGZ)
+
+
+release:
+	# Clean
+	$(GOCLEAN)
+	# Build for mac
+	#CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 $(GOBUILD)
+	# Build for arm
+#	$(GOCLEAN)
+	#CGO_ENABLED=0 GOOS=linux GOARCH=arm64 $(GOBUILD)
+	# Build for linux
+#	$(GOCLEAN)
+
+	GOOS=linux GOARCH=amd64 $(GOBUILD)
+	# Build for win
+#	$(GOCLEAN)
+#	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 $(GOBUILD).exe
+#	$(GOCLEAN)
+
+#windows64
+GOOS=windows GOARCH=amd64 go build -o bin/app-amd64.exe app.go
+#window32
+GOOS=windows GOARCH=386 go build -o bin/app-386.exe app.go
+# macos 64-bit
+GOOS=darwin GOARCH=amd64 go build -o bin/app-amd64-darwin app.go
+# macos 32-bit
+GOOS=darwin GOARCH=386 go build -o bin/app-386-darwin app.go
+
+# linux 64-bit
+GOOS=linux GOARCH=amd64 go build -o bin/app-amd64-linux app.go
+
+# linux 32-bit
+GOOS=linux GOARCH=386 go build -o bin/app-386-linux app.go
+docker:
+	GOOS=linux GOARCH=amd64 $(GOBUILD)
+	docker build -t drone/webhook . --tag registry.cn-shanghai.aliyuncs.com/zzf2001/drone-webhook
+	docker push registry.cn-shanghai.aliyuncs.com/zzf2001/drone-webhook
---
+++ plugin/plugin.go
@@ -0,0 +1,132 @@
+// Copyright 2019 the Drone Authors. All rights reserved.
+// Use of this source code is governed by the Blue Oak Model License
+// that can be found in the LICENSE file.
+
+package plugin
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"github.com/drone/drone-go/plugin/webhook"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// New returns a new webhook extension.
+func New(param1, param2 string) webhook.Plugin {
+	return &plugin{
+		// TODO replace or remove these configuration
+		// parameters. They are for demo purposes only.
+		param1: param1,
+		param2: param2,
+	}
+}
+
+type plugin struct {
+	// TODO replace or remove these configuration
+	// parameters. They are for demo purposes only.
+	param1 string
+	param2 string
+}
+
+func GenSign(secret string, timestamp int64) (string, error) {
+	stringToSign := fmt.Sprintf("%v", timestamp) + "\n" + secret
+
+	var data []byte
+	h := hmac.New(sha256.New, []byte(stringToSign))
+	_, err := h.Write(data)
+	if err != nil {
+		return "", err
+	}
+
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return signature, nil
+}
+func sendCard(status string, repoName string, repoLink string, commit string, build string) {
+	webhookUrl := os.Getenv("PLUGIN_WEBHOOK")
+	secret := os.Getenv("PLUGIN_SECRET")
+	baseUrl := os.Getenv("PLUGIN_BASE")
+	contentType := "application/json"
+	currentTime := time.Now().Unix()
+	sign, _ := GenSign(secret, currentTime)
+	sendData := `{
+	"timestamp": ` + strconv.FormatInt(currentTime, 10) + `,
+	"sign": "` + sign + `",
+	"msg_type": "interactive",
+    "card":{
+        "config":{
+            "wide_screen_mode":true
+        },
+        "elements":[
+            {
+                "tag":"hr"
+            },
+            {
+                "tag":"div",
+                "text":{
+                    "content":"👋 ` + status + ` \n⚒️️ [` + repoName + `](` + repoLink + `)\n👏️ [commit](` + commit + `)\n⚒️ [drone](` + baseUrl + build + `)",
+                    "tag":"lark_md"
+                }
+            }
+        ],
+        "header":{
+            "template":"blue",
+            "title":{
+                "content":"drone构建通知",
+                "tag":"plain_text"
+            }
+        }
+    }
+}`
+	resp, err := http.Post(webhookUrl, contentType, strings.NewReader(sendData))
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+	if resp.StatusCode == http.StatusCreated {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		jsonStr := string(body)
+		fmt.Println("Response: ", jsonStr)
+	} else {
+		body, _ := io.ReadAll(resp.Body)
+		fmt.Println("Get failed with error: ", resp.Status, string(body))
+	}
+}
+func (p *plugin) Deliver(ctx context.Context, req *webhook.Request) error {
+	if req.Event == webhook.EventBuild {
+		fmt.Printf("%+v\n", req)
+		link := req.Repo.Link
+		slug := req.Repo.Slug
+		commit := req.Build.Link
+		action := req.Action
+		build := slug + "/" + strconv.Itoa(int(req.Build.Number))
+		if action == webhook.ActionCreated {
+			sendCard(action, slug, link, commit, build)
+		}
+		if action == webhook.ActionUpdated {
+			if req.Build.Status == "success" {
+				fmt.Println("success")
+				sendCard(req.Build.Status, slug, link, commit, build)
+			}
+			if req.Build.Status == "failure" {
+				sendCard(req.Build.Status, slug, link, commit, build)
+			}
+		}
+	}
+	return nil
+}
